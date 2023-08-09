f5c598c7c751b31d55d88c09c141a6f0b8696f6d
joelbobai <90658305+joelbobai@users.noreply.github.com>
2023-08-09 07:35:11 +0100

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app");
-// Port Number
-const { PORT } = process.env;
-
-const startApp = () => {
-  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-};
-startApp();
