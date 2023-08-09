18947e6eecfa29497c53ce765f592b4051e8f14f
joelbobai <90658305+joelbobai@users.noreply.github.com>
2023-08-09 07:34:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-ff
+const app = require("./src/app");
+// Port Number
+const { PORT } = process.env;
+
+const startApp = () => {
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+};
+startApp();
