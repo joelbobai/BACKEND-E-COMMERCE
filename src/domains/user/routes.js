f90ce459d038e349d75cf5af64dbc65f0255ae0b
joelbobai <90658305+joelbobai@users.noreply.github.com>
2023-08-09 14:26:01 +0100

    Update routes.js

--- src/domains/user/routes.js
+++ src/domains/user/routes.js
@@ -67,7 +67,7 @@ router.post("/", async (req, res) => {
         path: "/",
         expires: new Date(Date.now() + 1000 * 60 * 55), // Set cookie to expire in 55 minutes
         httpOnly: true,
-        sameSite: "none",
+        sameSite: "lax",
       }
     );
     // Check if the 'connect.sid' cookie exists
