5068f30365705da4a8f1a714ac9996aa0e541680
joelbobai <90658305+joelbobai@users.noreply.github.com>
2023-08-09 15:24:48 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser());
 
 // Parse JSON Date
-app.use(express.json({ limit: "5mb" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
